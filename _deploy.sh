b05b025940b2f9d0a023ebe91c93584a8ba2a239
Garrett Grolemund <grolemund@gmail.com>
2018-12-04 10:23:14 -0600

    Points travis.ci to gh-pages branch (migrating from docs to gh-pages to avoid redundant travis builds)

--- _deploy.sh
+++ _deploy.sh
@@ -8,10 +8,13 @@ set -e
 git config --global user.email "grolemund@gmail.com"
 git config --global user.name "Garrett Grolemund"
 
-git clone https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
-cd book-output/docs/
+# clone the repository to the book-output directory
+git clone -b gh-pages \
+  https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git \
+  book-output
+cd book-output
 git rm -rf *
-cp -r ../../docs/* ./
+cp -r ../_book/* ./
 git add --all *
-git commit -m"Update the book" || true
-git push -q origin master
+git commit -m"Update the book"
+git push -q origin gh-pages
